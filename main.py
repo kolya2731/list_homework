93b9df9aa6076622d497780bc208648113622b12
Kolya Ostrovka <130283941+kolya2731@users.noreply.github.com>
2024-02-09 12:13:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-def bubble(ls: list) -> list:  # сортування методом бульбашки
+l = []
+n = int(input("n="))
+count = 0
+
+while n > count:
+    number = int(input("Enter number"))
+    l.append(number)
+    count += 1
+
+
+def bubble(ls: list) -> list:  #сортування методом бульбашки
     len_list = len(ls)
     for i in range(len_list):
         for r in range(len_list - i - 1):
             if ls[r] > ls[r + 1]:
                 ls[r], ls[r + 1] = ls[r + 1], ls[r]
-    return ls  # return sorted(ls)
+    return ls
 
 
-def sum_list(ls: list) -> int:  # знаходження суми списку
+def sum_list(ls: list) -> int:  #знаходження суми списку
     sum_ls = 0
     for number in ls:
         sum_ls += number
     return sum_ls  # return sum(ls)
+
+
+print(bubble(l))
+print(sum_list(l))
