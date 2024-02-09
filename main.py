0c01cf81d4252bc3aca659b83924ae7b70dba9e6
Kolya Ostrovka <mykolaostrovka27@gmail.com>
2024-02-09 10:22:13 +0200

    Solution

---
+++ main.py
@@ -0,0 +1,15 @@
+def bubble(ls: list) -> list:  #сортування методом бульбашки
+    len_list = len(ls)
+    for i in range(len_list):
+        for r in range(len_list - i - 1):
+            print(r)
+            if ls[r] > ls[r + 1]:
+                ls[r], ls[r + 1] = ls[r + 1], ls[r]
+    return ls
+
+
+def sum_list(ls: list) -> int:  #знаходження суми списку
+    sum_ls = 0
+    for number in ls:
+        sum_ls += number
+    return sum_ls  # return sum(ls)
